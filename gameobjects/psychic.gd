2fab7a2eb79d40c621775a55f2e790e2714501e7
davidgd72k <davidgd72k@gmail.com>
2026-02-17 04:29:35 +0100

    AnimationTree animation system implemented

--- gameobjects/psychic.gd
+++ gameobjects/psychic.gd
@@ -9,9 +9,11 @@ const WALK_ANIM = "go_"
 var debug_font = preload("uid://crk02q7wwi7ou")
 ## Player look direction angle.
 var current_look_angle: float
+var direction: Vector2
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var sprite_looker: SpriteLooker = $SpriteLooker
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,15 +23,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction = Input.get_vector("go_left", "go_right", "go_up", "go_down")
-	
-	if direction.length() > 0.0:
-		current_look_angle = direction.angle()
-		var anim_used = sprite_looker.get_animation_name_from_input_direction(direction.angle())
-		animation_player.play(WALK_ANIM + anim_used)
-	else:
-		animation_player.stop()
-	
 	if Input.is_action_just_pressed("use_power"):
 		# TODO: lanzar un rayo electrico psíquico.
 		print("animacion rayo")
@@ -37,26 +30,18 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("go_left", "go_right", "go_up", "go_down")
+	direction = Input.get_vector("go_left", "go_right", "go_up", "go_down").normalized()
 
 	# Decide walking animation for input direction angle.
-	#if direction.length() > 0.0:
-		#current_look_angle = direction.angle()
-		#var anim_used = sprite_looker.get_animation_name_from_input_direction(direction.angle())
-		#animation_player.play(WALK_ANIM + anim_used)
-	#else:
-		#animation_player.stop()
-
 	# Apply movement to character.
-	velocity = direction.normalized() * walk_speed
+	velocity = direction * walk_speed
+	update_blend(direction)
+	movement()
 	move_and_slide()
 
 
-func _draw():
-	for i in 8:
-		draw_line(Vector2.ZERO, Vector2(200, 0).rotated(PI/8 + i * PI/4), Color.GREEN, 5)
-		draw_string(debug_font, Vector2(150, 0).rotated(i * PI/4), str(i), 0, -1, 24, Color.WHITE)
-	draw_arc(Vector2.ZERO, 200, 0, 2*PI, 200, Color.RED, 5)
+func _draw() -> void:
+	pass
 
 
 func _input(event: InputEvent) -> void:
@@ -67,9 +52,20 @@ func _unhandled_input(event: InputEvent) -> void:
 	pass
 
 
+func update_blend(value) -> void:
+	if value == Vector2.ZERO:
+		return
+	
+	value.y *= -1
+	animation_tree.set("parameters/walk/blend_position", value)
+	animation_tree.set("parameters/idle/blend_position", value)
+
+
+func movement() -> void:
+	if is_zero_approx(velocity.length()):
+		state_machine.travel("idle")
+	else:
+		state_machine.travel("walk")
+
 func _on_animation_player_current_animation_changed(name: StringName) -> void:
-	$AnimationPlayer.play(name)
-
-
-func _on_sprite_looker_changed_direction(direction: String) -> void:
-	print_rich("[color=#a1c7ff]%s[/color]" % direction)
+	pass
